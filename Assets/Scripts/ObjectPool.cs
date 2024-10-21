8bcd82a95679c2f79687f212341e2c4d02c3b9a5
belfly1111 <belfly1111@naver.com>
2024-10-21 12:54:49 +0900

    [feat] ObjectPool 구현중
    [변경 사항]
    1. ObejctPool.cs / ObjectContainer.cs 를 생성하였습니다.

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject prefab;
+    private List<GameObject> pool = new List<GameObject>();
+    public int poolSize = 300;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        InitializePool();
+    }
+
+    private void InitializePool()
+    {
+
+    }
+
+    public GameObject Get()
+    {
+        
+    }
+
+    public void Release(GameObject obj)
+    {
+        
+    }
+}
+
