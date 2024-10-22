dd6b79878b057be1904b278c49999f124fe3ba5f
belfly1111 <belfly1111@naver.com>
2024-10-22 20:09:14 +0900

    [Add] ObjectPool.cs / ObjectCase
    [변경사항]
    1. ObjectPool을 제작하였습니다.
    2. ObjectPool에 들어갈 임시 prefab 'circle'을 제작하였습니다.
    3. Scene 이름을 변경하였습니다.

---
+++ Assets/Class/ObjectCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ObjectCase
+{
+    public int count;
+    public GameObject GO;
+    public Queue<GameObject> pool;
+}
--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    public GameObject prefab;
-    private List<GameObject> pool = new List<GameObject>();
-    public int poolSize = 300;
-
-    private void Awake()
-    {
-        
-    }
+    public ObjectCase[] objectCases;
+    public Dictionary<string, Queue<GameObject>> pool;
 
     void Start()
     {
@@ -20,17 +14,50 @@ public class ObjectPool : MonoBehaviour
 
     private void InitializePool()
     {
+        pool = new Dictionary<string, Queue<GameObject>>();
+        
+        for(int i = 0; i < objectCases.Length; i++)
+        {
+            string key = objectCases[i].GO.name;
 
+            if (!pool.ContainsKey(key))
+            {
+                pool.Add(key, new Queue<GameObject>());
+            }
+
+            for(int j = 0;  j < objectCases[i].count; j++)
+            {
+                GameObject curObj = Instantiate(objectCases[i].GO);
+                curObj.name = objectCases[i].GO.name;
+
+                curObj.SetActive(false);
+                pool[key].Enqueue(curObj);
+            }
+        }
     }
 
-    public GameObject Get()
+    public GameObject Get(string key)
     {
-        
+        // 해당 오브젝트가 존재하지 않을 경우 null을 리턴함. 
+        if (!pool.ContainsKey(key))
+        {
+            return null;
+        }
+
+        GameObject curObj = pool[key].Dequeue();
+        curObj.SetActive(true);
+        return curObj;
     }
 
     public void Release(GameObject obj)
     {
-        
+        // 해당 오브젝트가 존재하지 않을 경우 null을 리턴함. 
+        if (!pool.ContainsKey(obj.name))
+        {
+            return;
+        }
+
+        obj.SetActive(false);
+        pool[obj.name].Enqueue(obj);
     }
 }
-
